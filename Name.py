1498b12a2617a82f2dbe03bcec9ac18849cd128d
mahdi <mohammadmahdifarzanegan84@gmail.com>
2026-07-12 18:10:22 +0330

    Creat a file called Name file and add some code in it

---
+++ Name.py
@@ -0,0 +1,5 @@
+FullName = input("Please enter your full Name: ")
+ArrayFullName = FullName.split(" ")
+FirstName = ArrayFullName[0]
+LastName = ArrayFullName[1]
+print(f"Your First Name is {FirstName} and you Last Name is {LastName}")

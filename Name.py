f72cd5c56117c7f8d7f085b40e9c6a6347483f27
mahdi <mohammadmahdifarzanegan84@gmail.com>
2026-07-13 14:55:45 +0330

    edit

--- Name.py
+++ Name.py
@@ -3,3 +3,4 @@ ArrayFullName = FullName.split(" ")
 FirstName = ArrayFullName[0]
 LastName = ArrayFullName[1]
 print(f"Your First Name is {FirstName} and you Last Name is {LastName}")
+#kdlsfjlksdaj
